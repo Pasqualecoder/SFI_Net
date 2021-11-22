ada32cc1434eaeb323ca91db9fa1f973b99aeba5
Pasqualecoder <pasqualemuraca2003@gmail.com>
2021-11-22 21:46:44 +0100

    miglioramento nav bar

--- sito/js/script.js
+++ sito/js/script.js
@@ -1,5 +1,9 @@
-function funzione() {
-    document.getElementById("ilmiotitolo").innerHTML = "Hello world";
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsiveTopBar() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	    x.className += " responsive";
+	} else {
+	    x.className = "topnav";
+	}
 }
-
-console.log("Hello world");
